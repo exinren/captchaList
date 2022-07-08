7f3062ca5f22e086c78242d9b08580e9ea72e44f
xiaoer <380780735@qq.com>
2022-07-08 23:13:45 +0800

    迁移代码。

---
+++ server/captchas/click_captcha.go
@@ -0,0 +1,276 @@
+package captchas
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/wenlng/go-captcha/captcha"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"server/tools"
+	"strconv"
+	"strings"
+	"time"
+)
+
+/**
+ * @Description: demo
+ * @param w
+ * @param r
+ */
+func Demo(w http.ResponseWriter, r *http.Request) {
+	sessid := time.Now().UnixNano() / 1e6
+	t, _ := template.ParseFiles(GetPWD() + "/view/demo.html")
+	_ = t.Execute(w, map[string]interface{}{"sessid": sessid})
+}
+
+/**
+ * @Description: Example
+ * @param w
+ * @param r
+ */
+func GetCaptchaData(w http.ResponseWriter, r *http.Request) {
+	capt := captcha.GetCaptcha()
+
+	dots, b64, tb64, key, err := capt.Generate()
+	if err != nil {
+		bt, _ := json.Marshal(map[string]interface{}{
+			"code": 1,
+			"message": "GenCaptcha err",
+		})
+		_, _ = fmt.Fprintf(w, string(bt))
+		return
+	}
+	writeCache(dots, key)
+	bt, _ := json.Marshal(map[string]interface{}{
+		"code": 0,
+		"image_base64": b64,
+		"thumb_base64": tb64,
+		"captcha_key": key,
+	})
+	_, _ = fmt.Fprintf(w, string(bt))
+}
+
+/**
+ * @Description: Verify where the user clicks on the image
+ * @param w
+ * @param r
+ */
+func CheckCaptcha(w http.ResponseWriter, r *http.Request) {
+	code := 1
+	_ = r.ParseForm()
+	dots := r.Form.Get("dots")
+	key := r.Form.Get("key")
+	if dots == "" || key == "" {
+		bt, _ := json.Marshal(map[string]interface{}{
+			"code":    code,
+			"message": "dots or key param is empty",
+		})
+		_, _ = fmt.Fprintf(w, string(bt))
+		return
+	}
+
+	cacheData := readCache(key)
+	if cacheData == "" {
+		bt, _ := json.Marshal(map[string]interface{}{
+			"code":    code,
+			"message": "illegal key",
+		})
+		_, _ = fmt.Fprintf(w, string(bt))
+		return
+	}
+	src := strings.Split(dots, ",")
+
+	var dct map[int]captcha.CharDot
+	if err := json.Unmarshal([]byte(cacheData), &dct); err != nil {
+		bt, _ := json.Marshal(map[string]interface{}{
+			"code":    code,
+			"message": "illegal key",
+		})
+		_, _ = fmt.Fprintf(w, string(bt))
+		return
+	}
+
+	chkRet := false
+	if (len(dct)*2) == len(src) {
+		for i, dot := range dct {
+			j := i * 2
+			k := i * 2 + 1
+			sx, _ := strconv.ParseFloat(fmt.Sprintf("%v", src[j]), 64)
+			sy, _ := strconv.ParseFloat(fmt.Sprintf("%v", src[k]), 64)
+
+			// 检测点位置
+			// chkRet = captchas.CheckPointDist(int64(sx), int64(sy), int64(dot.Dx), int64(dot.Dy), int64(dot.Width), int64(dot.Height))
+
+			// 校验点的位置,在原有的区域上添加额外边距进行扩张计算区域,不推荐设置过大的padding
+			// 例如：文本的宽和高为30，校验范围x为10-40，y为15-45，此时扩充5像素后校验范围宽和高为40，则校验范围x为5-45，位置y为10-50
+			chkRet = captcha.CheckPointDistWithPadding(int64(sx), int64(sy), int64(dot.Dx), int64(dot.Dy), int64(dot.Width), int64(dot.Height), 5)
+			if !chkRet {
+				break
+			}
+		}
+	}
+
+	if chkRet {
+		// 通过校验
+		code = 0
+	}
+
+	bt, _ := json.Marshal(map[string]interface{}{
+		"code": code,
+	})
+	_, _ = fmt.Fprintf(w, string(bt))
+	return
+}
+
+/**
+ * @Description: Write Cache，“Redis” is recommended
+ * @param v
+ * @param file
+ */
+func writeCache(v interface{}, file string) {
+	bt, _ := json.Marshal(v)
+	month := time.Now().Month().String()
+	cacheDir := getCacheDir() + month + "/"
+	_ = os.MkdirAll(cacheDir, 0660)
+	file = cacheDir + file + ".json"
+	logFile, _ := os.OpenFile(file, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0644)
+	defer logFile.Close()
+	// 检查过期文件
+	//checkCacheOvertimeFile()
+	_, _ = io.WriteString(logFile, string(bt))
+}
+
+/**
+ * @Description: Read Cache，“Redis” is recommended
+ * @param file
+ * @return string
+ */
+func readCache(file string) string {
+	month := time.Now().Month().String()
+	cacheDir := getCacheDir() + month + "/"
+	file = cacheDir + file + ".json"
+
+	if !checkFileIsExist(file) {
+		return ""
+	}
+
+	bt, err := ioutil.ReadFile(file)
+	err = os.Remove(file)
+	if err == nil {
+		return string(bt)
+	}
+	return ""
+}
+
+/**
+ * @Description: Calculate the distance between two points
+ * @param sx
+ * @param sy
+ * @param dx
+ * @param dy
+ * @param width
+ * @param height
+ * @return bool
+ */
+func checkDist(sx, sy, dx, dy, width, height int64) bool {
+	return sx >= dx &&
+		sx <= dx + width &&
+		sy <= dy &&
+		sy >= dy - height
+}
+
+/**
+ * @Description: Get cache dir path
+ * @return string
+ */
+func getCacheDir() string  {
+	return GetPWD() + "/.cache/"
+}
+
+
+/**
+ * @Description: Get pwd dir path
+ * @return string
+ */
+func GetPWD() string {
+	path, err := os.Getwd()
+	if err != nil {
+		return ""
+	}
+	return path
+}
+
+/**
+ * @Description: Check file exist
+ * @param filename
+ * @return bool
+ */
+func checkFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+
+/**
+ * @Description: 启动定时任务, 5分钟执行一次
+ */
+func RunTimedTask()  {
+	ticker := time.NewTicker(time.Minute * 5)
+	go func() {
+		for range ticker.C {
+			checkCacheOvertimeFile()
+		}
+	}()
+}
+
+/**
+ * @Description: 检查缓存超时文件， 30分钟
+ */
+func checkCacheOvertimeFile()  {
+	files, files1, _ := listDir(getCacheDir())
+	for _, table := range files1 {
+		temp,_,_ := listDir(table)
+		for _,temp1 := range temp{
+			files = append(files, temp1)
+		}
+	}
+
+	for _, file := range files {
+		t := tools.GetFileCreateTime(file)
+		ex := time.Now().Unix() - t
+		if ex > (60 * 30) {
+			_ = os.Remove(file)
+		}
+	}
+}
+
+/**
+ * @Description: 获取目录文件列表
+ * @param dirPth
+ * @return files
+ * @return files1
+ * @return err
+ */
+func listDir(dirPth string) (files []string,files1 []string, err error) {
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return nil,nil, err
+	}
+
+	PthSep := string(os.PathSeparator)
+	for _, fi := range dir {
+		if fi.IsDir() { // 忽略目录
+			files1 = append(files1, dirPth+PthSep+fi.Name())
+			_, _, _ = listDir(dirPth + PthSep + fi.Name())
+		} else {
+			files = append(files, dirPth+PthSep+fi.Name())
+		}
+	}
+	return files, files1, nil
+}
+
